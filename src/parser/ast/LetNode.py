44f418465aabcb975fe5641f59c2be06965b7555
Stefan Nožinić <stefan@lugons.org>
2016-03-17 21:46:06 +0100

    Fix init arguments in LetNode class

--- src/parser/ast/LetNode.py
+++ src/parser/ast/LetNode.py
@@ -1,9 +1,9 @@
-﻿import .ExprNode import * 
-import .ASTNode import * 
+﻿from .ExprNode import * 
+from .ASTNode import * 
 
 class LetNode(ASTNode):
     """represents a LET <var> = <expr> statement"""
-    def __init__(self:LetNode, var: str, expr: ExprNode):
+    def __init__(self, var: str, expr: ExprNode):
         self.var = var
         self.expr = expr
 
