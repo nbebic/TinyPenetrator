85577406d57047e63063513fd43a9b7e3130a1f6
nbebic <nikola.bebic99@gmail.com>
2016-03-17 21:58:29 +0100

    Add lexer tests

---
+++ src/tests/TestLexer.py
@@ -0,0 +1,12 @@
+﻿import unittest
+from ..lexer import *
+
+class TestLexer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_basic(self):
+        e = lex('2 3+7 "ma + ()gija" (2+3*4) LET S = X')
+        self.assertListEqual(['2', '3', '+', '7', '"ma + ()gija"', '(', '2', '+', '3', '*', '4', ')', 'LET', 'S', '=', 'X'],e)
+
--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,2 +1,3 @@
-
+﻿
 from .TestExpression import *
+from .TestLexer import *
