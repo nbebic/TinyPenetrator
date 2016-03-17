fb269ba92bff87cd9ada4042c842c1770bdd82a3
Stefan Nožinić <stefan@lugons.org>
2016-03-17 21:49:51 +0100

    Add test for brackets

--- src/tests/TestExpression.py
+++ src/tests/TestExpression.py
@@ -26,3 +26,11 @@ class TestExpression(unittest.TestCase):
         self.assertEqual(e.left.number, 1)
         self.assertEqual(e.right.number, 2)
         self.assertEqual(e.operator, "*")
+
+    def test_addition_brackets(self):
+        e = parse_expr(["1", "+", "(", "2", "+", "3", ")"])
+        self.assertEqual(e.left.number, 1)
+        self.assertEqual(e.right.left.number, 2)
+        self.assertEqual(e.right.right.number, 3)
+        self.assertEqual(e.right.operator, "+")
+        self.assertEqual(e.operator, "+")
