b32b7da4455e309c138619aceeb81361a27fb53d
nbebic <nikola.bebic99@gmail.com>
2016-03-18 11:20:24 +0100

    Make ExprNode codegen (not tested)

--- src/parser/ast/ExprNode.py
+++ src/parser/ast/ExprNode.py
@@ -1,10 +1,44 @@
-
+﻿
 from .ASTNode import ASTNode
 
 class ExprNode(ASTNode):
-    
     right = None
     left = None 
 
-    operator = None 
+    operator = None
+    def codegen(self):
+        prolog = """
+        \tPOP HL
+        \tPOP DE"""
 
+        epilog = """
+        \tPUSH HL
+        """
+
+        add = """
+        \tADD HL, DE
+        """
+
+        sub = """
+        \tLD F, 0
+        \tSBC HL, DE
+        """
+        
+        mul = """
+        """
+
+        div = """
+        """
+
+        left.codegen()
+        right.codegen()
+        asm_output(prolog)
+        if operator == '+':
+            asm_output(add)
+        elif operator == '-':
+            asm_output(sub)
+        elif operator == '*':
+            asm_output(mul)
+        elif operator == '/':
+            asm_output(div)
+        asm_output(epilog)
